6ff3dc08799f10c84f74cd9f78bc44c29b0599c2
Ethan Kim <ek.35543@gmail.com>
2020-09-16 18:25:15 -0400

    Initial Commit

---
+++ YourTurn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YourTurn
+//
+//  Created by Ethan K. on 9/16/20.
+//  Copyright © 2020 Ethan Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
